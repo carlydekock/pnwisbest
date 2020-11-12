e41b2b8214b88d949fc52bc49c3ae93f1b6d5ca3
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2020-11-11 17:13:10 -0800

    updated styling and added js files

---
+++ js/app2.js
@@ -0,0 +1,5 @@
+'user strict'
+
+let userLocation = prompt('Where do you live?');
+
+document.write('<h3>' + userLocation + '</h3>');
---
+++ js/app3.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let month = new Date();
+
+let monthnow = month.getMonth();
+
+let greeting2;
+
+if (monthnow > 8) {
+    greeting2 = 'Happy Fall!';
+} else if (monthNow > 4) {
+    greeting2 = 'Happy Summer!';
+} else if (monthNow > 1) {
+    greeting2 = 'Happy Spring!';
+} else {
+    greeting2 = 'Happy Winter!';
+}
+
+document.write('<h3>' + greeting2 + '</h3>');
